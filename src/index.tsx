0552bdce532abd2bbcaba09e28db4c9c32462061
YOOYONGHYUN <65952639+YOOYONGHYUN@users.noreply.github.com>
2025-04-05 03:36:36 +0900

    Add src/index.tsx entry point

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
